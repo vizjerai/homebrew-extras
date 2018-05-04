7a4e61782cb01240caff0f42cc52ddb3637d0273
Andrew Assarattanakul <andrew@bendyworks.com>
2018-05-04 11:42:14 -0500

    simplify formula by using git repo because it contains links to dependencies.

--- pngwolf-zopfli.rb
+++ pngwolf-zopfli.rb
@@ -1,8 +1,8 @@
 class PngwolfZopfli < Formula
   desc "PNG minimizer which uses a genetic algorithm to find optimial PNG scanline filter combinations."
   homepage "https://github.com/jibsen/pngwolf-zopfli"
-  url "https://github.com/jibsen/pngwolf-zopfli/archive/v1.1.2.tar.gz"
-  sha256 "59a757a5bab16ff7c8433b37682997bab564ae0c976a07d931632c0508103d85"
+  head "https://github.com/jibsen/pngwolf-zopfli.git"
+
   depends_on "cmake" => :build
 
   def install
